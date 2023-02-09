18fc282816f32ec2f3f86875d74525f2540f7224
Yujin <wjd2000db@gmail.com>
2023-02-09 04:32:09 -0500

    Add Databinding

--- app/src/main/java/com/example/a01/MainActivity.java
+++ app/src/main/java/com/example/a01/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.a01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.a01.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding
+                = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
+
+
 }
