f2d277dbb60d23086b234a489df3c23a641c7507
Maísa Wolff Resplande <maisawres@gmail.com>
2023-02-10 01:24:47 -0500

    Include Itinerary fragment for second screen.

---
+++ app/src/main/java/com/example/a01/Itinerary.java
@@ -0,0 +1,46 @@
+package com.example.a01;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.a01.databinding.ItineraryBinding;
+
+public class Itinerary extends Fragment {
+    private ItineraryBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = ItineraryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.backToTrips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
