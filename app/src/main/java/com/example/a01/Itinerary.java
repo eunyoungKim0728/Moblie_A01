937bdaa2f99fa9af3984222580ba5d9c664e214f
Yujin <wjd2000db@gmail.com>
2023-02-10 21:28:44 -0500

    Add spinner to Itinerary

--- app/src/main/java/com/example/a01/Itinerary.java
+++ app/src/main/java/com/example/a01/Itinerary.java
@@ -9,9 +9,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
-
+import android.widget.Spinner;
 
 import com.example.a01.databinding.ItineraryBinding;
 
@@ -21,10 +23,39 @@ public class Itinerary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //databinding
         ItineraryBinding binding = DataBindingUtil.setContentView(this,R.layout.itinerary);
 
-        TextView textView = binding.dayText;
+        Spinner mySpinner = binding.daySpinner;
         ImageView imageView = binding.cntowerImg;
+
+        //spinner
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.day_spinner,
+                                                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(adapter);
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int positionitem = mySpinner.getSelectedItemPosition();
+
+                switch(positionitem){
+                    case 1:
+                        startActivity(new Intent(getApplicationContext(),Itinerary.class));
+                    case 2:
+                        startActivity(new Intent(getApplicationContext(),ItineraryDay2.class));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
 
 
 
