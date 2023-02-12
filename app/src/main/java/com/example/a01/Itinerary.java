a472b6089176716d0ac31f4bc713ed047f6dc93b
Joyonghihae <rladms010@gmail.com>
2023-02-11 23:56:14 -0500

    create the command

--- app/src/main/java/com/example/a01/Itinerary.java
+++ app/src/main/java/com/example/a01/Itinerary.java
@@ -1,3 +1,9 @@
+/*
+ * FILE				:Itinerary.java
+ * PROGRAMMER		:Eunyoung Kim, Yujin Jeong, Hyewon Lee, Maísa Wolff Resplande
+ * FIRST VERSION	:2023-02-11
+ * DESCRIPTION		:A program aimed at tracking travel routes
+ */
 package com.example.a01;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +24,13 @@ import com.example.a01.databinding.ItineraryBinding;
 
 public class Itinerary extends AppCompatActivity {
 
+
+    /*  -- Method Header Comment
+	Name	:	onCreate
+	Purpose :	Function that runs when clicked
+	Inputs	:	savedInstanceState	Bundle
+	Outputs	:	NONE
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +45,15 @@ public class Itinerary extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),Packing.class));
             }
         });
-
-
-
-
-
     }
 
+
+    /*  -- Method Header Comment
+	Name	:	onCreateOptionsMenu
+	Purpose :	Method that runs when a menu is clicked
+	Inputs	:	menu	Menu
+	Outputs	:	NONE
+    */
     @SuppressLint("RestrictedApi")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,6 +63,14 @@ public class Itinerary extends AppCompatActivity {
         return true;
     }
 
+
+
+    /*  -- Method Header Comment
+	Name	:	onOptionsItemSelected
+	Purpose :	Method for displaying a list
+	Inputs	:	item	MenuItem
+	Outputs	:	NONE
+    */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         boolean result =false;
--- app/src/main/java/com/example/a01/Packing.java
+++ app/src/main/java/com/example/a01/Packing.java
@@ -1,3 +1,9 @@
+/*
+ * FILE				:Itinerary.java
+ * PROGRAMMER		:Eunyoung Kim, Yujin Jeong, Hyewon Lee, Maísa Wolff Resplande
+ * FIRST VERSION	:2023-02-11
+ * DESCRIPTION		: Programs that show Packing and can run on that screen
+ */
 package com.example.a01;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +22,13 @@ import com.example.a01.databinding.PackingBinding;
 
 public class Packing extends AppCompatActivity{
 
+
+    /*  -- Method Header Comment
+	Name	:	onCreate
+	Purpose :	Function that runs when clicked
+	Inputs	:	savedInstanceState	Bundle
+	Outputs	:	NONE
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +44,12 @@ public class Packing extends AppCompatActivity{
     }
 
 
+    /*  -- Method Header Comment
+	Name	:	onCreateOptionsMenu
+	Purpose :	Method that runs when a menu is clicked
+	Inputs	:	menu	Menu
+	Outputs	:	NONE
+    */
     @SuppressLint("RestrictedApi")
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,6 +59,14 @@ public class Packing extends AppCompatActivity{
         return true;
     }
 
+
+
+    /*  -- Method Header Comment
+	Name	:	onOptionsItemSelected
+	Purpose :	Method for displaying a list
+	Inputs	:	item	MenuItem
+	Outputs	:	NONE
+    */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         boolean result =false;
