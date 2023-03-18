581b04829774abd6485df1d3515ba3cafc79bec1
Maísa Wolff Resplande <maisawres@gmail.com>
2023-03-18 15:57:55 -0400

    Add TODO to use recycler view.

--- app/src/main/java/com/example/a01/ResultList.java
+++ app/src/main/java/com/example/a01/ResultList.java
@@ -3,6 +3,8 @@ package com.example.a01;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.MatrixCursor;
@@ -10,9 +12,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -54,7 +58,20 @@ public class ResultList extends AppCompatActivity {
 
 
         TextView textView = binding.resultNameList;
+        textView.setText("Check who's already packaged to your trip!" );
+
+
+        // TODO Implement recycler view instead of list view
+        /*RecyclerView recyclerView = binding.resultCheckList;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));*/
+
         ListView listView = binding.resultCheckList;
+        PlannerDatabase db = PlannerDatabase.getInstance(this);
+        List<String> list = new ArrayList<>();
+        list = db.getUsersDAO().getAllUsersName();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
 
 
        /* Intent intent = getIntent();
@@ -66,16 +83,38 @@ public class ResultList extends AppCompatActivity {
         String checkBox5 = intent.getStringExtra("checkBox5");
         String checkBox6 = intent.getStringExtra("checkBox6");*/
 
-        textView.setText("Check who's already packaged to your trip!" );
+
 
         //textView.setText(name +"'s Packing List!" );
 
-        PlannerDatabase db = PlannerDatabase.getInstance(this);
-        List<String> list = new ArrayList<>();
-        list = db.getUsersDAO().getAllUsersName();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
 
     }
+
+    // TODO Write method for ListItemHolder
+
+    // TODO Write method of ListAdapter for recycler view
+    /*private class ListAdapter extends RecyclerView.Adapter {
+
+        ListBinding binding = null;
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            binding = ListItemBinding.inflate(getLayoutInflater());
+//            View view = getLayoutInflater().inflate(R.layout.list_item, parent, false);
+            return new ListItemHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            String item = myList.get(position);
+            ((ListItemHolder)holder).getMyTextView().setText(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return myList.size();
+        }
+    }*/
 }
